5fe51b30f2ec6a31b5cdd51494ef22fcc31ed699
Andrea Brunelli <andrea.brunelli93@gmail.com>
2021-02-03 14:57:14 +0100

    andamento regionale funzionante

--- static/js/andamento_nazionale.js
+++ static/js/andamento_nazionale.js
@@ -1,24 +1,24 @@
 remap_codes_regioni = [
-    { "nome" : "Abruzzo", "codice": 65, "popolazione" : 1322247, "color": "" },
-    { "nome": "Basilicata", "codice": 77, "popolazione" : 570365, "color": ""},
-    { "nome": "Calabria", "codice": 78, "popolazione" : 1965128, "color": "" },
-    { "nome": "Campania", "codice": 72, "popolazione" :  5839084, "color": ""},
-    { "nome": "Emilia-Romagna", "codice": 45, "popolazione" :  4448841, "color": ""},
-    { "nome": "Friuli-Venezia Giulia", "codice": 36, "popolazione" : 1217872, "color": "" },
-    { "nome": "Lazio", "codice": 62, "popolazione" :  5898124, "color": ""},
-    { "nome": "Liguria", "codice": 42, "popolazione" : 1565307, "color": "" },
-    { "nome": "Lombardia", "codice": 25, "popolazione" : 10018806 , "color": ""},
-    { "nome": "Marche", "codice": 57, "popolazione" : 1538055, "color": "" },
-    { "nome": "Molise", "codice": 67, "popolazione" : 310449, "color": "" },
-    { "nome": "Piemonte", "codice": 21, "popolazione" : 4392526, "color": "" },
-    { "nome": "Puglia", "codice": 75, "popolazione" : 4063888, "color": "" },
-    { "nome": "Sardegna", "codice": 88, "popolazione" : 1653135, "color": "" },
-    { "nome": "Sicilia", "codice": 82, "popolazione" : 5056641, "color": "" },
-    { "nome": "Toscana", "codice": 52, "popolazione" : 3742437, "color": "" },
-    { "nome": "Trentino-Alto Adige", "codice": 32, "popolazione" : 1062860, "color": "" },
-    { "nome": "Umbria", "codice": 55, "popolazione" : 888908, "color": "" },
-    { "nome": "Valle dAosta" , "codice": 23, "popolazione" : 126883, "color": "" },
-    { "nome": "Veneto", "codice": 34, "popolazione" : 4907529, "color": "" }
+    { "nome": "Abruzzo", "codice": 65, "popolazione": 1322247, "color": "#03071e" },
+    { "nome": "Basilicata", "codice": 77, "popolazione": 570365, "color": "#370617" },
+    { "nome": "Calabria", "codice": 78, "popolazione": 1965128, "color": "#6a040f" },
+    { "nome": "Campania", "codice": 72, "popolazione": 5839084, "color": "#9d0208" },
+    { "nome": "Emilia-Romagna", "codice": 45, "popolazione": 4448841, "color": "#d00000" },
+    { "nome": "Friuli-Venezia Giulia", "codice": 36, "popolazione": 1217872, "color": "#dc2f02" },
+    { "nome": "Lazio", "codice": 62, "popolazione": 5898124, "color": "#f48c06" },
+    { "nome": "Liguria", "codice": 42, "popolazione": 1565307, "color": "#faa307" },
+    { "nome": "Lombardia", "codice": 25, "popolazione": 10018806, "color": "#ffba08" },
+    { "nome": "Marche", "codice": 57, "popolazione": 1538055, "color": "#003049" },
+    { "nome": "Molise", "codice": 67, "popolazione": 310449, "color": "#d62828" },
+    { "nome": "Piemonte", "codice": 21, "popolazione": 4392526, "color": "#f77f00" },
+    { "nome": "Puglia", "codice": 75, "popolazione": 4063888, "color": "#fcbf49" },
+    { "nome": "Sardegna", "codice": 88, "popolazione": 1653135, "color": "#eae2b7" },
+    { "nome": "Sicilia", "codice": 82, "popolazione": 5056641, "color": "#007f5f" },
+    { "nome": "Toscana", "codice": 52, "popolazione": 3742437, "color": "#2b9348" },
+    { "nome": "Trentino-Alto Adige", "codice": 32, "popolazione": 1062860, "color": "#80b918" },
+    { "nome": "Umbria", "codice": 55, "popolazione": 888908, "color": "#bfd200" },
+    { "nome": "Valle dAosta", "codice": 23, "popolazione": 126883, "color": "#d4d700" },
+    { "nome": "Veneto", "codice": 34, "popolazione": 4907529, "color": "#ffff3f" }
 ]
 
 var data_andamento = [];
@@ -65,98 +65,108 @@ andamento_nazionale.map((item) => {
     probabili_positivi.push(item.terapia_intensiva * avg);
 });
 
+try {
+    var ctx1 = document.getElementById('andamento_indici').getContext('2d');
+    var chart = new Chart(ctx1, {
+        // The type of chart we want to create
+        type: 'line',
 
-var ctx = document.getElementById('andamento_indici').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: data_andamento,
-        datasets: [{
-            label: 'Totale Positivi',
-            borderColor: '#4680ff',
-            fill: false,
-            pointRadius: 1,
-            data: totale_positivi_adj,
-            order: totale_positivi
-        },
-        {
-            label: 'Terapie intensive',
-            borderColor: '#ff5252',
-            fill: false,
-            pointRadius: 1,
-            data: terapia_intensiva_adj
-        },
-        ]
-
-    },
-
-    // Configuration options go here
-    options: {}
-});
-
-var ctx = document.getElementById('andamento_probabile').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: data_andamento,
-        datasets: [{
-            label: 'Totale Positivi',
-            borderColor: '#4680ff',
-            fill: false,
-            pointRadius: 1,
-            data: totale_positivi,
-        },
-        {
-            label: 'Terapie intensive',
-            borderColor: '#ff5252',
-            fill: false,
-            pointRadius: 1,
-            data: terapia_intensiva
-        },
-        {
-            label: 'Probabili positivi',
-            borderColor: '#ffba57',
-            fill: false,
-            pointRadius: 1,
-            data: probabili_positivi
-        },
-        ]
-
-    },
-
-    // Configuration options go here
-    options: {}
-});
-
-var ctx = document.getElementById('andamento_regioni').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-
-    //
-    data: {
-        labels: data_andamento,
-        datasets: remap_codes_regioni.map((regione)=>{
-            return{
-                label: regione.nome,
+        // The data for our dataset
+        data: {
+            labels: data_andamento,
+            datasets: [{
+                label: 'Totale Positivi',
                 borderColor: '#4680ff',
                 fill: false,
                 pointRadius: 1,
-                data: andamento_regioni_storico.filter((x) => x['codice_regione'] == regione.codice).map(function (item) { return item['totale_positivi'] / regione.popolazione })
+                data: totale_positivi_adj,
+                order: totale_positivi
+            },
+            {
+                label: 'Terapie intensive',
+                borderColor: '#ff5252',
+                fill: false,
+                pointRadius: 1,
+                data: terapia_intensiva_adj
+            },
+            ]
+
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+} catch (e) {
+    console.log('andamento indici non esiste')
+}
+
+
+
+try {
+    var ctx2 = document.getElementById('andamento_probabile').getContext('2d');
+    var chart = new Chart(ctx2, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: data_andamento,
+            datasets: [{
+                label: 'Totale Positivi',
+                borderColor: '#4680ff',
+                fill: false,
+                pointRadius: 1,
+                data: totale_positivi,
+            },
+            {
+                label: 'Terapie intensive',
+                borderColor: '#ff5252',
+                fill: false,
+                pointRadius: 1,
+                data: terapia_intensiva
+            },
+            {
+                label: 'Probabili positivi',
+                borderColor: '#ffba57',
+                fill: false,
+                pointRadius: 1,
+                data: probabili_positivi
+            },
+            ]
+
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+} catch (e) {
+    console.log("andamento probabile non esite")
+}
+
+try {
+    var ctx3 = document.getElementById('andamento_regioni').getContext('2d');
+    var chart = new Chart(ctx3, {
+        // The type of chart we want to create
+        type: 'line',
+
+        data: {
+            labels: data_andamento,
+            datasets: remap_codes_regioni.map((regione) => {
+                return {
+                    label: regione.nome,
+                    borderColor: regione.color,
+                    fill: false,
+                    pointRadius: 1,
+                    data: andamento_regioni_storico.filter((x) => x['codice_regione'] == regione.codice).map(function (item) { return item['totale_positivi'] / regione.popolazione })
                 }
-        })
-    },
+            })
+        },
+        options: {}
+    });
 
-
-
-    options: {}
-});
+} catch (e) {
+    console.log('andamento regioni non esiste')
+}
 
 
 
